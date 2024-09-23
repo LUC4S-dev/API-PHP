059f56f160521a2088390aa08e20e15ba7b85011
LUC4S-dev <lucaslenisbenicio@hotmail.com>
2024-09-22 22:10:36 -0300

    first commit

---
+++ src/Core/core.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Core;
+
+class Core
+{
+    public static function dispatch(array $routes)
+    {
+        print_r($routes);
+
+    }
+}
---
+++ src/Routes/main.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Route;
+
+Route::get('/', 'HomeController@index');
+Route::post('/', 'HomeController@index');
+Route::put('/', 'HomeController@index');
+Route::delete('/', 'HomeController@index');
+
